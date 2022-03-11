2daa6eb03863c822b83ef6cc23bd7a08626082ad
Pierre Cherbero <cameleon24@outlook.fr>
2022-03-11 20:05:59 +0100

    i18next gallery and style

--- src/components/bio/Bio.js
+++ src/components/bio/Bio.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { Trans, withTranslation } from 'react-i18next';
+import LangSelector from './LangSelector';
+
 // import { useLocation } from 'react-router-dom';
 import "./bio.css";
 
 const Bio = () => {
-
   const url_affiche =
     "https://res.cloudinary.com/dnpddlkrq/image/upload/v1641565779/blues_rules_nvzdsp.jpg";
 
   return (
     <>
+      <div>
+      </div>
+      <LangSelector />
       <div className="header-portrait">
       <div className="title">
         <h1 className="bord_haut">MS NICKKI</h1>
@@ -20,20 +25,7 @@ const Bio = () => {
       <div className="ui two column doubling stackable grid container">
         <div className="column text-one">
           <div className="ui segment text">
-            MS Nickki est une "blueswoman" née a Holly Springs (Mississippi),
-            berceau de la musique noire américaine dont elle est une
-            représentante aujourd'hui. <br/>
-            Elle vous envoie sur la planète "Blues"
-            avec un talent vocal impressionnant et un charme naturel qui ne peut
-            vous laisser insensible... MS Nickki chante un blues profond alliant
-            la puissance du gospel (répertoire qu'elle connaît particulièrement bien)
-            et la douceur de la soul: une alchimie musicale typique de Memphis,
-            Tennessee, cette immense ville au riche passé musical dans laquelle
-            elle réside depuis une dizaine d'années.<br/>
-            Ses influences majeures
-            sont des chanteuses telles Denise LaSalle, Aretha Franklin, Tina
-            Turner, Barbara Carr et des chanteurs originaire de Memphis tels que
-            Johnnie Taylor et Al Green.
+      <Trans>BIOONE</Trans>
           </div>
         </div>
         <div className="tof-one">
@@ -53,21 +45,12 @@ const Bio = () => {
         <div className="column bio text-two">
           <div className="ui segment text">
             <div className="combo">
-            MS Nickki: chant lead<br/>
-            Florian Royo: guitare<br/>
-            Pierre Cherbero: claviers choeurs<br/>
-            Julien Dubois: basse<br/>
-            Eric Boréave: batterie choeurs<br/>
+            <Trans>BAND</Trans>
             <br/>
+                      <br/>
+
             </div>
-            Le Quintett tourne régulièrement en Europe depuis une dizaine
-            d'années et s'est produit sur des grandes scènes comme Cahors Blues
-            Festival, Blues sur Seine, Beautiful Swamp Blues Festival, Cameros
-            Blues Festival (Espagne). Dublin Blues Festival (Irlande), Blues
-            Rules Festival (Suisse) et biens d'autres. Sur scène, c'est une onde
-            de choc qui se propage ! Elle aime chanter et partager son amour
-            pour le Blues et la Vie! Elle sait le montrer et "emmener" les gens,
-            c'est l'école américaine du show...
+              <Trans>BIOTWO</Trans>
           </div>
         </div>
       </div>
@@ -89,4 +72,4 @@ const Bio = () => {
   );
 };
 
-export default Bio;
+export default withTranslation()(Bio);
